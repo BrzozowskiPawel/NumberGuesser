d907b2ba2d6b1a0ca74840ea2d3f8a61a76f0d9d
Paweł Brzozowski <mac@192.168.1.4>
2021-10-16 20:30:07 +0200

    added lives func.

--- NumberGuesserApp/ViewController.swift
+++ NumberGuesserApp/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var range: Int = 2
+    var lives: Int = 1
+    var hints = true
 
+    @IBOutlet weak var rangeLabel: UILabel!
+    @IBOutlet weak var rangeDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var numberOfLives: UILabel!
+    @IBOutlet weak var numberOfLivesDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var hintsLabel: UILabel!
+    @IBOutlet weak var hintsDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rangeStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func livesStepper(_ sender: UIStepper) {
+        if sender.value > 1 {
+            if sender.value < 5 {
+                numberOfLivesDescriptionLabel.text = "Choose the number of lives."
+                numberOfLives.text = "\(Int(sender.value).description) ♥️"
+                lives = Int(sender.value)
+            }
+            else {
+                numberOfLivesDescriptionLabel.text = "Thats max. number ❤️‍🔥"
+                numberOfLives.text = "\(Int(sender.value).description) ♥️"
+                lives = Int(sender.value)
+            }
+        }
+        else {
+            numberOfLivesDescriptionLabel.text = "It cannot be lower 💔"
+            numberOfLives.text = "\(Int(sender.value).description) ♥️"
+            lives = Int(sender.value)
+        }
+    }
+    
+    @IBAction func hintSwitch(_ sender: UISwitch) {
+    }
+    
+    
 }
 
